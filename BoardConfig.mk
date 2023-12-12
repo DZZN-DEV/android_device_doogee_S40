256a3539116fe4bca0455b1de91356b1cc3612e5
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2023-12-12 21:33:16 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,3 +91,19 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+# SHRP Configuration
+
+SHRP_PATH := device/doogee/S40
+SHRP_MAINTAINER := DZZN-DEV
+SHRP_DEVICE_CODE := S40
+SHRP_REC_TYPE := Normal
+SHRP_DEVICE_TYPE := A_Only
+SHRP_DARK := true
+SHRP_EDL_MODE := 0
+
+
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
