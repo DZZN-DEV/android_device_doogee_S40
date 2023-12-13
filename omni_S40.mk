6aaf6b8c1336b7730a72105468f20424ea3f37ae
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2023-12-13 06:15:29 +0100

    Update omni_S40.mk

--- omni_S40.mk
+++ omni_S40.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/pb/config/common.mk)
+#$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_DEVICE := S40
 PRODUCT_NAME := omni_S40
