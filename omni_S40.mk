65d4dad3d45ea318c1dfc93dbb0451fa3bef2b04
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2023-12-13 02:41:28 +0100

    Update omni_S40.mk

--- omni_S40.mk
+++ omni_S40.mk
@@ -4,18 +4,19 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 $(call inherit-product, vendor/pb/config/common.mk)
-$(call inherit-product, device/doogee/S40/device.mk)
+
 PRODUCT_DEVICE := S40
 PRODUCT_NAME := omni_S40
 PRODUCT_BRAND := DOOGEE
-PRODUCT_MODEL := S40
-PRODUCT_MANUFACTURER := doogee
+PRODUCT_MODEL := $(PRODUCT_DEVICE)
+PRODUCT_MANUFACTURER := $(shell echo $(PRODUCT_BRAND) | tr '[:upper:]' '[:lower:]')
 PRODUCT_GMS_CLIENTID_BASE := android-doogee
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="S40_RU-user 9 P00610 1562833591 release-keys"
+$(call inherit-product, device/$(shell echo $(PRODUCT_BRAND) | tr '[:upper:]' '[:lower:]')/$(PRODUCT_DEVICE)/device.mk)
 
-BUILD_FINGERPRINT := DOOGEE/S40_RU/S40:9/P00610/1562833591:user/release-keys
+BUILD_FINGERPRINT := DOOGEE/S40/S40:9/P00610/20231213/User/release-key
