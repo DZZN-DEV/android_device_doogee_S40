c9543cdbb5e6bd7bbe9c379fba161e3491aa890a
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2023-12-13 01:35:38 +0100

    Update omni_S40.mk

--- omni_S40.mk
+++ omni_S40.mk
@@ -4,26 +4,15 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-
-# Inherit from S40 device
 $(call inherit-product, device/doogee/S40/device.mk)
-
-# Inherit common product files.
-$(call inherit-product, vendor/pb/config/common.mk)
-
 PRODUCT_DEVICE := S40
 PRODUCT_NAME := omni_S40
 PRODUCT_BRAND := DOOGEE
 PRODUCT_MODEL := S40
 PRODUCT_MANUFACTURER := doogee
-
 PRODUCT_GMS_CLIENTID_BASE := android-doogee
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
