4c153145faa5c82accefe9a2e11d4af1fc540597
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2023-12-13 02:02:32 +0100

    Update omni_S40.mk

--- omni_S40.mk
+++ omni_S40.mk
@@ -6,7 +6,7 @@
 #
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 $(call inherit-product, device/doogee/S40/device.mk)
 PRODUCT_DEVICE := S40
 PRODUCT_NAME := omni_S40
